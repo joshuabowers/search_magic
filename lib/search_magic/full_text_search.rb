5b13f4a94f2e0de9c0bc43a8ee5b6087f9fc4f30
Joshua Bowers <joshua@avandus.com>
2011-03-18 19:28:26 -0700

    Slowly fleshing out the capabilities of the FullTextSearch portion of the library.

--- lib/search_magic/full_text_search.rb
+++ lib/search_magic/full_text_search.rb
@@ -1,8 +1,19 @@
 module SearchMagic
   module FullTextSearch
     module ClassMethods
+      def self.extended(receiver)
+        receiver.send :before_save, :update_searchable_values
+      end
+      
       def searchable_fields(*field_names)
+        class_attribute :_searchable_fields, :instance_writer => false
+        send(:_searchable_fields=, Hash[*field_names.map {|field_name| [field_name, true]}.flatten])
         field :_searchable_values
+      end
+      
+      private
+      
+      def update_searchable_values
       end
     end
   
--- spec/unit/search_magic/full_text_search_spec.rb
+++ spec/unit/search_magic/full_text_search_spec.rb
@@ -3,8 +3,28 @@ require 'spec_helper'
 describe SearchMagic::FullTextSearch do
   describe "searchable_fields" do
     context "when called with :serial and :status" do
+      it "should create a class attribute called :_searchable_fields and make it a Hash" do
+        Part.should respond_to(:_searchable_fields)
+        Part._searchable_fields.should be_a(Hash)
+      end
+      
       it "should create a field called :_searchable_values" do
         Part.fields.keys.should include("_searchable_values")
+      end
+      
+      it "should add :serial and :status to :_searchable_fields" do
+        Part._searchable_fields.keys.should include(:serial, :status) 
+      end
+      
+      describe "before_save" do
+        before do
+          @part = Part.new(:status => "available", :serial => "1234abcd")
+        end
+        
+        it "should run the update_searchable_values callback" do
+          @part.should_receive(:update_searchable_values)
+          @part.save
+        end
       end
     end
   end
