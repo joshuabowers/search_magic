98f6d63ca2351c2a61920a3f38d0ccb6ea2eb1ef
Joshua Bowers <joshua@avandus.com>
2012-01-30 00:42:29 -0800

    Updating the railtie to hopefully get it to work correctly.

--- lib/search_magic/railtie.rb
+++ lib/search_magic/railtie.rb
@@ -1,5 +1,9 @@
+require 'search_magic'
+require 'rails'
 module SearchMagic
   class Railtie < Rails::Railtie
+    railtie_name :search_magic
+    
     rake_tasks do
       Dir["tasks/**/*.rake"].each { |ext| load ext }
     end
